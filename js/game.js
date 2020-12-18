54d5228df454bc1011ed088d57a4c0d3e68cc802
TomSuzuki-No.13 <tomsuzuki2633@gmail.com>
2020-12-19 06:13:30 +0900

    [add] background

--- js/game.js
+++ js/game.js
@@ -15,13 +15,15 @@ let game;
 class gameVariable {
     flg = 0;
     time = 0;
+    animationTime = 0;
     ctx = null;
     dinnerList = null;
     todayDinner = "";
     key = "dinner";
     imgHontai = new Image();
-    imgCapsule = new Image();
     capsuleObj = new Array();
+    imgTodayCapsule = new Image();
+    imgBackground = new Image();
 
     // init
     constructor(ctx, dinnerList) {
@@ -30,6 +32,7 @@ class gameVariable {
 
         // hontai
         this.imgHontai.src = "./img/hontai.png";
+        this.imgBackground.src = "./img/background.png";
 
         // add normal
         for (let i = 0; i < 48; i++) this.capsuleObj.push(new capsuleObject("./img/capsule_1.png"));
@@ -43,11 +46,15 @@ class gameVariable {
         let rnd = random(allPoint);
         if (rnd <= PAR_DINNER) {
             this.key = "dinner";
+            this.imgTodayCapsule = new capsuleObject("./img/capsule_1.png");
         } else if (rnd <= PAR_DINNER + PAR_LIKE) {
             this.key = "like";
+            this.imgTodayCapsule = new capsuleObject("./img/capsule_2.png");
         } else {
             this.key = "eating_out";
+            this.imgTodayCapsule = new capsuleObject("./img/capsule_3.png");
         }
+        this.imgTodayCapsule.setPoint(-999, -999);
         let n = random(Object.keys(this.dinnerList[this.key]).length - 1);
         this.todayDinner = this.dinnerList[this.key][n];
         console.log("今日の夜ごはんは", this.todayDinner, "です。");
@@ -87,11 +94,30 @@ class capsuleObject {
         this.img.src = src;
     }
 
+    // point
+    setPoint(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
     // draw
     draw(time) {
         game.grotation(this.img, this.x, this.y, 50, 50, time / 12 + this.angle);
     }
 }
+
+// start animation
+function startAnimation(b) {
+    if (!b) return;
+    game.flg = 1;
+    game.animationTime = game.time;
+}
+
+// animation
+function animation(time) {
+    let y = 250 + 118.0 * easeOutBounce(time / 120);
+    game.imgTodayCapsule.setPoint(228, y);
+};
 
 
 // game init
@@ -108,6 +134,10 @@ function gameMain() {
     // run
     game.time++
 
+    // animation
+    if (game.flg == 0) startAnimation(getClick());
+    else if (game.flg == 1) animation(game.time - game.animationTime);
+
     // draw
     gameDraw();
 
@@ -122,10 +152,14 @@ function gameDraw() {
     game.ctx.fillStyle = "#FFFFFF";
     game.ctx.fillRect(0, 0, 320, 640);
 
+    // backgroungd img
+    game.gcopy(game.imgBackground, 160, 320, 320, 640);
+
     // capsule
     for (var i in game.capsuleObj) {
         game.capsuleObj[i].draw(game.time);
     }
+    game.imgTodayCapsule.draw(game.time / 5);
 
     // hontai
     game.gcopy(game.imgHontai, 160, 220, 400, 380);
@@ -136,4 +170,20 @@ function gameDraw() {
 // for random
 function random(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function easeOutBounce(time) {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+    let x = Math.min(time, 1);
+
+    if (x < 1 / d1) {
+        return n1 * x * x;
+    } else if (x < 2 / d1) {
+        return n1 * (x -= 1.5 / d1) * x + 0.75;
+    } else if (x < 2.5 / d1) {
+        return n1 * (x -= 2.25 / d1) * x + 0.9375;
+    } else {
+        return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    }
 }
--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ window.addEventListener('load', function () {
 function initialize() {
 	screenCanvas = document.getElementById('canvas');
 	screenCanvas.addEventListener('mousemove', mouseMove, true);
+	screenCanvas.addEventListener('click', onClick, false);
 	let ctx = screenCanvas.getContext('2d');
 	loadTextFile("./data/dinner.json", function (result) {
 		json = JSON.parse(result);
@@ -19,12 +20,26 @@ function initialize() {
 
 // mouse move event
 function mouseMove(event) {
-	mouse.x = event.clientX - screenCanvas.offsetLeft + 320 / 2;
-	mouse.y = event.clientY - screenCanvas.offsetTop + 640 / 2;
+	mouse.x = event.clientX - screenCanvas.offsetLeft;
+	mouse.y = event.clientY - screenCanvas.offsetTop;
 }
 
 // mouse point
 function point() {
 	this.x = 0;
 	this.y = 0;
+	this.click = false;
+}
+
+// mouse click
+function onClick() {
+	console.log(mouse.x, mouse.y);
+	mouse.click = true;
+}
+
+// get click
+function getClick() {
+	let b = mouse.click;
+	mouse.click = false;
+	return b;
 }
